9c4b76dfaca9cd1dcf283fdc1a5869f6413e547c
Lilieh <sheislilieh@gmail.com>
2021-11-23 22:29:34 -0600

    classone

---
+++ assets/js/base.js
@@ -0,0 +1,34 @@
+/*
+	Ejercicio 1:
+	Pedir 2 variables por prompt
+	Si a > b dividir a entre b
+	Si a < b sumar a más b
+	si a == b multiplicar ambos numeros
+	Imprimir el resultado en consola
+*/
+
+const a = parseInt (prompt ("elige un valor para a"))
+const b = parseInt (prompt ("elige un valor para b"))
+console.log(isNaN(a), isNaN(b))
+
+if(isNaN (a) ===false && isNaN (b) === false){
+console.log (a,b)
+    if (a > b){
+    console.log("división:", a/b)
+}else if(a < b) {
+    console.log("suma:", a+b)
+}else if (a === b){
+    console.log("multiplicación:", a*b)
+}
+}else{
+    console.error("Algún valor no es tipo number")
+}
+
+
+//for ( i=1; i<=10; i++){
+//template literals console.log ("tabla del" + (i) + ".")//
+//console.log(`tabla del` ${i})
+//for(j = 1 ; j <= 10 ; j++){
+//    console.log(`${i} x ${j} = ${i*j}`)
+//}
+//}
