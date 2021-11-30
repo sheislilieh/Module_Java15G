b273a2ac704ea5f38f82b9b87a100d65e181da39
Lilieh <sheislilieh@gmail.com>
2021-11-29 19:30:21 -0600

    classfour

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,40 +1,29 @@
-//let saludo = "hola"
-//for(i=0; i <= saludo.length; i++ ){
-//console.log(saludo[i])
-//}
-
-//  pedir al usuario un string
-//  contar las vocales y las consonantes
-// imprimir en consola
-// 2 vocales y 2 consonantes
-
-const userWord = prompt("introduce una palabra")
-
-if(userWord.length > 0){
-    let vowels = 0
-    let consonant=0
-    for(i = 0; i < userWord.length; i++){
-        let letras = userWord[i].toLowerCase()
-        if (letras !== " "){
-            if(
-                letras === "a" || 
-                letras === "e" || 
-                letras === "i" || 
-                letras === "o" || 
-                letras === "u"
-                ){    
-            vowels++
-        }else{
-            consonant ++
-        } 
-    }
-    }           
-    console.log( `${vowels} vocales y ${consonant} consonantes`)
-}else{
-    console.log("inserta una palabra valida")   
-} 
+//
+//	Pedir al usuario una palabra
+//	Devolver la palabra capitalizada
+//	p.ej. 'galAxiA'
+//	Ouput -> 'Galaxia'
+//
   
-    
-        
-       
-   
+//let wordCap = prompt("introduce una palabra").toLowerCase().trim()
+//let wordCap1= wordCap[0].toUpperCase()+wordCap.substring(1)
+//console.log(wordCap1)
+
+//Pedir al usuario una oración (permitir espacios)
+//	Capitalizar cada una de las palabras de la oración
+//	p.ej. 'En una galaxia muy muy lejana'
+//	Ouput -> 'En Una Galaxia Muy Muy Lejana'
+
+const word = prompt("introduce una frase").toLowerCase().trim()
+let wordCap3 = " "
+for(i = 0; i< word.length; i++){
+    if(word[i]== " "){
+     //   console.log(word[i+1])
+        wordCap3 += " "+word[i+1].toUpperCase()
+        i = i+1
+    }else{
+        wordCap3= wordCap3+word[i]
+    }
+}
+wordCap3= wordCap3.substring(0,1).toUpperCase() + wordCap3.substring(1)
+console.log(wordCap3)
